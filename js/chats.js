d267d27345141aced31c01d40f96624b01576710
lina0987 <li.st99@web.de>
2021-10-27 09:58:08 +0200

    zyklische Aktualisierung

---
+++ js/chats.js
@@ -0,0 +1,17 @@
+/**
+ * Aktualisiert die Seite jede Sekunde
+ */
+window.setInterval(function () {
+    location.reload()
+}, 1000);
+
+/**
+ * Nachrichten versenden
+ */
+function sendMessage(){
+    
+}
+
+/**
+ * NAchrichten empfangen
+ */
