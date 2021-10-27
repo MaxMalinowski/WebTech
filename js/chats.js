86fd2ba5fa701f77c044389dce285c5557a9b752
Max Malinowski <max.malinowski@outlook.de>
2021-10-27 18:38:32 +0200

    hacky solution to send and update chat - to be improved

--- js/chats.js
+++ js/chats.js
@@ -1,48 +1,66 @@
+var messagesOnServer
+
 /**
- * Aktualisiert die Seite jede Sekunde
+ * Update every second to check if new messages available
  */
-function update() {
-  window.setInterval(function () {
-    location.reload();
-    console.log("updated");
-  }, 1000);
+window.setInterval(function () {
+    recieveMessages();
+    appendMessages();
+}, 1000);
+
+/**
+ * Retrieve messages from the server
+ */
+function recieveMessages() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            messagesOnServer = JSON.parse(xmlhttp.responseText);
+        }
+    };
+    let url = window.chatServer + window.chatCollectionId + "/message/Jerry";
+    xmlhttp.open("GET", url, true);
+    xmlhttp.setRequestHeader(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM1MDAyNDI5fQ.sXgsn2HsU0vZspogYuMhWLiWBNIZV5OGPBF-cdsNogQ"
+    );
+    xmlhttp.send();
 }
 
 /**
- * Nachrichten versenden
+ * Create new message elements in chat if not yet displayed
+ */
+function appendMessages() {
+    var fieldsetElement = document.getElementById("chat");
+    fieldsetElement.textContent = '';
+    
+    messagesOnServer.forEach(msg => {
+      let newElement = document.createElement("p");
+      newElement.innerText = msg["msg"];
+      fieldsetElement.appendChild(newElement);
+    });
+}
+
+/**
+ * Send new message to the server
  */
 function sendMessage() {
-  let messageText = document.getElementById("message").value;
-  let xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
-      console.log("done...");
-    }
-  };
-  var url = window.chatServer + "/chat/" + window.chatCollectionId + "/message";
-  xmlhttp.open("POST", url, true);
+    let messageText = { to: "Jerry", message: document.getElementById("message").value };
+    let messageJson = JSON.stringify(messageText);
 
-  let data = {
-    message: messageText,
-    to: "Jerry",
-  };
-  let jsonString = JSON.stringify(data);
-  xmlhttp.send(jsonString);
-}
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
+            console.log("An error occured while sending the message ...");
+        }
+    };
 
-/**
- * Nachrichten empfangen
- */
-function receiveMessage() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      let data = JSON.parse(xmlhttp.responseText);
-      console.log(data);
-    }
-  };
-  var url =
-    window.chatServer + "/chat/" + window.chatCollectionId + "/message/Jerry";
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
+    let url = window.chatServer + window.chatCollectionId + "/message";
+    xmlhttp.open("POST", url, false);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.setRequestHeader(
+        "Authorization",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM1MDAyNDI5fQ.sXgsn2HsU0vZspogYuMhWLiWBNIZV5OGPBF-cdsNogQ"
+    );
+    xmlhttp.send(messageJson);
 }
