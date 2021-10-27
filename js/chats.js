2fb90377a6fed8a72615e73975eed50dff7a3346
lina0987 <li.st99@web.de>
2021-10-27 11:40:19 +0200

    erste Versuche zum Chat

--- js/chats.js
+++ js/chats.js
@@ -1,17 +1,35 @@
 /**
  * Aktualisiert die Seite jede Sekunde
  */
-window.setInterval(function () {
-    location.reload()
-}, 1000);
+function update() {
+  window.setInterval(function () {
+    location.reload();
+    console.log("updated");
+  }, 1000);
+}
 
 /**
  * Nachrichten versenden
  */
-function sendMessage(){
-    
+function sendMessage() {
+  let message = document.getElementById("message").value;
+  var url =
+    window.chatServer +
+    "/" +
+    window.chatCollectionId +
+    "/chat/" +
+    message.value;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", url, true);
+  xmlhttp.send();
 }
 
 /**
- * NAchrichten empfangen
+ * Nachrichten empfangen
  */
+function receiveMessage() {
+  var url = window.chatServer + "/" + window.chatCollectionId + "/chat";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
