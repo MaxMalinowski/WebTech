986b3e2157490da28e2c494ab1ca865a45ff2f05
Max Malinowski <max.malinowski@outlook.de>
2021-10-29 15:28:28 +0200

    extract constants and animation to seperate file

--- js/chats.js
+++ js/chats.js
@@ -1,5 +1,5 @@
-var messagesDisplayed = 0
-var messagesOnServer = []
+var messagesDisplayed = 0;
+var messagesOnServer = [];
 
 /**
  * Update every second to check if new messages available
@@ -7,7 +7,7 @@ var messagesOnServer = []
 window.setInterval(function () {
     recieveMessages();
     if (messagesOnServer.length > messagesDisplayed) {
-      appendMessages(messagesOnServer.slice(messagesDisplayed));
+        appendMessages(messagesOnServer.slice(messagesDisplayed));
     }
 }, 1000);
 
@@ -15,18 +15,16 @@ window.setInterval(function () {
  * Retrieve messages from the server
  */
 function recieveMessages() {
+    let url = window.chatServer + window.chatCollectionId + "/message/Tom";
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             messagesOnServer = JSON.parse(xmlhttp.responseText);
         }
     };
-    let url = window.chatServer + window.chatCollectionId + "/message/Jerry";
+
     xmlhttp.open("GET", url, true);
-    xmlhttp.setRequestHeader(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM1MDAyNDI5fQ.sXgsn2HsU0vZspogYuMhWLiWBNIZV5OGPBF-cdsNogQ"
-    );
+    xmlhttp.setRequestHeader("Authorization", "Bearer " + window.authToken);
     xmlhttp.send();
 }
 
@@ -34,70 +32,60 @@ function recieveMessages() {
  * Create new message elements in chat if not yet displayed
  */
 function appendMessages(messagesToAppend) {
-  
     var fieldsetElement = document.getElementById("chat");
-
-    messagesToAppend.forEach(msg => {
-      newElement = buildMessage(msg["from"], msg["msg"], msg["time"])
-      fieldsetElement.appendChild(newElement);
-      messagesDisplayed++
+    messagesToAppend.forEach((msg) => {
+        newElement = buildMessage(msg["from"], msg["msg"], msg["time"]);
+        fieldsetElement.appendChild(newElement);
+        messagesDisplayed++;
     });
-    removeLoadingIndicator()
-    scrollUpdate()
+
+    removeLoadingIndicator();
+    scrollUpdate();
 }
 
 /**
  * Build a param-element representing a message in the chat
  */
 function buildMessage(msgFrom, msgText, msgDate) {
-  let newElement = document.createElement("table");
-  let newMsgName = document.createElement("td");
-  let newMsgText = document.createElement("td");
-  let newMsgDate = document.createElement("td");
+    let newElement = document.createElement("table");
+    let newMsgName = document.createElement("td");
+    let newMsgText = document.createElement("td");
+    let newMsgDate = document.createElement("td");
 
-  newElement.classList.add("chat-message")
-  newMsgName.classList.add("chat-message-name")
-  newMsgText.classList.add("chat-message-text")
-  newMsgDate.classList.add("chat-message-date")
+    newElement.classList.add("chat-message");
+    newMsgName.classList.add("chat-message-name");
+    newMsgText.classList.add("chat-message-text");
+    newMsgDate.classList.add("chat-message-date");
 
-  newMsgName.innerText = msgFrom + ":"
-  newMsgText.innerText = msgText
-  newMsgDate.innerText = (new Date(msgDate)).toLocaleString("en-DE")
+    newMsgName.innerText = msgFrom + ":";
+    newMsgText.innerText = msgText;
+    newMsgDate.innerText = new Date(msgDate).toLocaleString("en-DE");
 
-  newElement.appendChild(newMsgName)
-  newElement.appendChild(newMsgText)
-  newElement.appendChild(newMsgDate)
+    newElement.appendChild(newMsgName);
+    newElement.appendChild(newMsgText);
+    newElement.appendChild(newMsgDate);
 
-  return newElement
+    return newElement;
 }
 
 /**
  * Send new message to the server
  */
 function sendMessage() {
-    let messageText = { to: "Jerry", message: document.getElementById("message").value };
+    let url = window.chatServer + window.chatCollectionId + "/message";
+    let messageText = { to: "Tom", message: document.getElementById("message").value };
     let messageJson = JSON.stringify(messageText);
 
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status != 200) {
-            console.log("An error occured while sending the message ...");
-        }
-    };
-
-    let url = window.chatServer + window.chatCollectionId + "/message";
     xmlhttp.open("POST", url, false);
     xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.setRequestHeader(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM1MDAyNDI5fQ.sXgsn2HsU0vZspogYuMhWLiWBNIZV5OGPBF-cdsNogQ"
-    );
+    xmlhttp.setRequestHeader("Authorization", "Bearer " + window.authToken);
     xmlhttp.send(messageJson);
-    
-    document.getElementById('message').value = ''
-    insertLoadingIndicator()
-    scrollUpdate()
-    return false
+
+    document.getElementById("message").value = "";
+    insertLoadingIndicator();
+    scrollUpdate();
+    return false;
 }
 
 /**
@@ -106,33 +94,33 @@ function sendMessage() {
 function insertLoadingIndicator() {
     var fieldsetElement = document.getElementById("chat");
 
-    let newDotContainer = document.createElement("div")
-    let newFlashingDots = document.createElement("div")
+    let newDotContainer = document.createElement("div");
+    let newFlashingDots = document.createElement("div");
 
-    newDotContainer.id = "dot-container"
-    newDotContainer.classList.add("dot-container")
-    newFlashingDots.classList.add("dot-flashing")
+    newDotContainer.id = "dot-container";
+    newDotContainer.classList.add("dot-container");
+    newFlashingDots.classList.add("dot-flashing");
 
-    newDotContainer.appendChild(newFlashingDots)
-    fieldsetElement.appendChild(newDotContainer)
+    newDotContainer.appendChild(newFlashingDots);
+    fieldsetElement.appendChild(newDotContainer);
 }
 
 /**
  * Remove a loading indicator
  */
 function removeLoadingIndicator() {
-  console.log("removing dots ...")
-  let exists = document.getElementById("dot-container")
-  if (exists != undefined) {
-    document.getElementById("dot-container").remove()
-  }
+    console.log("removing dots ...");
+    let exists = document.getElementById("dot-container");
+    if (exists != undefined) {
+        document.getElementById("dot-container").remove();
+    }
 }
 
 /**
  * Scroll down to the latest message
  */
 function scrollUpdate() {
-  console.log("updating scroll ...")
-  var chatElement = document.getElementById("chat");
-  chatElement.scrollTop = chatElement.scrollHeight;
+    console.log("updating scroll ...");
+    var chatElement = document.getElementById("chat");
+    chatElement.scrollTop = chatElement.scrollHeight;
 }
