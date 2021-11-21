5faa8a1b04a14578a6841598c408d5a1ba72ba8b
lina0987 <li.st99@web.de>
2021-11-21 15:39:31 +0100

    fixed all red underlined ||not working yet

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-register',
@@ -7,43 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  benutzername: string;
-  passwort: string;
-  bestatigtPW: string;
+  benutzername: HTMLElement | null = null;
+  passwort: HTMLElement | null = null;
+  bestatigtPW: string = '';
 
-  messageUser: string;
-  messagePassword: string;
-  messageConfirmedPW: string;
+  messageUser: string = '';
+  messagePassword: string = '';
+  messageConfirmedPW: string = '';
 
-  hiddenUN: boolean;
-  hiddenPW: boolean;
-  hiddenCPW: boolean;
+  hiddenUN: boolean = true;
+  hiddenPW: boolean = true;
+  hiddenCPW: boolean = true;
 
-  usernameLengthStatus : any;
- usernameExistencyStatus: any ;
- passwordLengthStatus : any;
- passwordConfirmationStatus : any;
- timer: any
+  usernameLengthStatus: any = { Value: false };
+  usernameExistencyStatus: any = { Value: false };
+  passwordLengthStatus: any = { Value: false };
+  passwordConfirmationStatus: any = { Value: false };
+  timer: any = null;
 
-  public constructor(private router: Router) {
-    this.benutzername = '';
-    this.passwort = '';
-    this.bestatigtPW = '';
-
-    this.messageUser = 'Message User';
-    this.messagePassword = 'Message Password';
-    this.messageConfirmedPW = 'Message Confirmed';
-
-    this.hiddenUN = true;
-    this.hiddenPW = true;
-    this.hiddenCPW = true;
-
-    this.usernameLengthStatus = { Value: false };
-    this.usernameExistencyStatus = { Value: false };
-    this.passwordLengthStatus = { Value: false };
-    this.passwordConfirmationStatus = { Value: false };
-    this.timer = null;
-  }
+  public constructor(
+    private router: Router,
+    private backendService: BackendService
+  ) {}
 
   public ngOnInit(): void {}
 
@@ -52,142 +38,170 @@ export class RegisterComponent implements OnInit {
   }
 
   public create(): void {
-    this.router.navigate(['/friends']);
+    if (
+      !this.messageUser &&
+      !this.messagePassword &&
+      !this.messageConfirmedPW
+    ) {
+      this.router.navigate(['/friends']);
+    }
   }
 
   //*********************************************************************copied*************************** */
 
-
-
-/**
- * Function to check to verify the username after 1 sec of no input
- * --> used in HTML files
- */
-function checkUsername() {
-    usernameElement = document.getElementById("username");
-    setStatus(usernameLengthStatus, usernameElement, "grey");
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        isUsernameValid(usernameElement);
-    }, 1000);
-}
-
-/**
- * Function to check, wheter the supplied password us least 8 characters
- * --> used in HTML files
- */
-function checkPassword() {
-    var passwordElement = document.getElementById("password");
-
-    if (passwordElement.value.length < 8) {
-        setStatus(passwordLengthStatus, passwordElement, "red");
-    } else {
-        setStatus(passwordLengthStatus, passwordElement, "green");
+  /**
+   * Function to check to verify the username after 1 sec of no input
+   * --> used in HTML files
+   */
+  public checkUsername(): void {
+    this.benutzername = document.getElementById('benutzername');
+    if (this.benutzername) {
+      this.setStatus(this.usernameLengthStatus, this.benutzername, 'grey');
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        if (this.benutzername) {
+          this.isUsernameValid(this.benutzername);
+        }
+      }, 1000);
     }
-}
+  }
 
-/**
- * Function to check, wheter the supplied confirmation password matches the password
- * --> used in HTML files
- */
-function checkConfirmedPassword() {
-    var passwordElement = document.getElementById("password");
-    var confirmationElement = document.getElementById("confirm");
+  /**
+   * Function to check, wheter the supplied password us least 8 characters
+   * --> used in HTML files
+   */
+  public checkPassword(): void {
+    var passwordElement = document.getElementById('passwort');
 
-    if (passwordElement.value === confirmationElement.value) {
-        setStatus(passwordConfirmationStatus, confirmationElement, "green");
-    } else {
-        setStatus(passwordConfirmationStatus, confirmationElement, "red");
+    if (passwordElement?.nodeValue && passwordElement.nodeValue.length < 8) {
+      this.setStatus(this.passwordLengthStatus, passwordElement, 'red');
+    } else if (passwordElement) {
+      this.setStatus(this.passwordLengthStatus, passwordElement, 'green');
     }
-}
+  }
 
-/**
- * Function to check, whether the supplied values for username and password are ok
- * --> used in HTML files
- */
-function checkForm() {
+  /**
+   * Function to check, wheter the supplied confirmation password matches the password
+   * --> used in HTML files
+   */
+  public checkConfirmedPassword(): void {
+    var passwordElement = document.getElementById('passwort');
+    var confirmationElement = document.getElementById('bestatigtPW');
+
+    if (
+      passwordElement?.nodeValue &&
+      confirmationElement?.nodeValue &&
+      passwordElement.nodeValue === confirmationElement.nodeValue
+    ) {
+      this.setStatus(
+        this.passwordConfirmationStatus,
+        confirmationElement,
+        'green'
+      );
+    } else if (confirmationElement) {
+      this.setStatus(
+        this.passwordConfirmationStatus,
+        confirmationElement,
+        'red'
+      );
+    }
+  }
+
+  /**
+   * Function to check, whether the supplied values for username and password are ok
+   * --> used in HTML files
+   */
+  public checkForm(): void {
     let valid = new Set([
-        usernameLengthStatus.Value,
-        usernameExistencyStatus.Value,
-        passwordLengthStatus.Value,
-        passwordConfirmationStatus.Value,
+      this.usernameLengthStatus.Value,
+      this.usernameExistencyStatus.Value,
+      this.passwordLengthStatus.Value,
+      this.passwordConfirmationStatus.Value,
     ]);
 
     if (valid.size === 1 && valid.has(true)) {
-        return true;
+      return;
     } else {
-        return writeAlert();
+      this.writeAlert();
     }
-}
+  }
 
-/**
- * Function to set the border color of a specific element to a specific color
- */
-function setStatus(flag, element, color) {
-    element.style.border = "2px solid " + color;
+  /**
+   * Function to set the border color of a specific element to a specific color
+   */
+  public setStatus(
+    flag: { Value: boolean },
+    element: HTMLElement,
+    color: string
+  ): void {
+    element.style.border = '2px solid ' + color;
 
-    if (color == "green") {
-        flag.Value = true;
+    if (color == 'green') {
+      flag.Value = true;
     } else {
-        flag.Value = false;
+      flag.Value = false;
     }
-}
+  }
 
-/**
- * Fucntion to check whether the supplied username is at least 3 characters long and doesn't already exist
- */
-function isUsernameValid(usernameElement) {
-    if (usernameElement.value.length < 3) {
-        setStatus(usernameLengthStatus, usernameElement, "red");
+  /**
+   * Fucntion to check whether the supplied username is at least 3 characters long and doesn't already exist
+   */
+  public isUsernameValid(usernameElement: HTMLElement): void {
+    if (usernameElement.nodeValue && usernameElement.nodeValue.length < 3) {
+      this.setStatus(this.usernameLengthStatus, usernameElement, 'red');
     } else {
-        setStatus(usernameLengthStatus, usernameElement, "green");
-        doesUserExist(usernameElement);
+      this.setStatus(this.usernameLengthStatus, usernameElement, 'green');
+      this.doesUserExist(usernameElement);
     }
-}
+  }
 
-/**
- * Function to execute a server request to check whether a username already exists or not
- */
-function doesUserExist(usernameElement) {
-    var url = window.chatServer + "/" + window.chatCollectionId + "/user/" + usernameElement.value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 204) {
-            setStatus(usernameExistencyStatus, usernameElement, "red");
-        } else if (xmlhttp.status == 404) {
-            setStatus(usernameExistencyStatus, usernameElement, "green");
-        }
-    };
+  /**
+   * Function to execute a server request to check whether a username already exists or not
+   */
+  public async doesUserExist(usernameElement: HTMLElement): Promise<void> {
+    const userlist: Promise<string[]> = this.backendService.listUsers();
+    (await userlist).forEach((element) => {
+      if (this.benutzername?.nodeValue === element) {
+        this.setStatus(this.usernameExistencyStatus, usernameElement, 'red');
+      } else {
+        this.setStatus(this.usernameExistencyStatus, usernameElement, 'green');
+      }
+    });
+  }
 
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
-
-/**
- * Function for Alerts
- */
-function writeAlert() {
-    let message = "";
-
-    if (!usernameExistencyStatus.Value) {
-        message = message + "\nUsername already exists";
+  /**
+   * Function for Alerts
+   */
+  public writeAlert(): void {
+    if (!this.usernameExistencyStatus.Value) {
+      this.messageUser = this.messageUser + '\nUsername already exists';
     }
 
-    if (!usernameLengthStatus.Value) {
-        message = message + "\nUsername must consist of at least 3 characters";
+    if (!this.usernameLengthStatus.Value) {
+      this.messageUser =
+        this.messageUser + '\nUsername must consist of at least 3 characters';
     }
 
-    if (!passwordLengthStatus.Value) {
-        message = message + "\nPassword must consist of at least 8 characters";
+    if (!this.passwordLengthStatus.Value) {
+      this.messagePassword =
+        this.messagePassword +
+        '\nPassword must consist of at least 8 characters';
     }
 
-    if (!passwordConfirmationStatus.Value) {
-        message = message + "\nPasswords do not match";
+    if (!this.passwordConfirmationStatus.Value) {
+      this.messageConfirmedPW =
+        this.messageConfirmedPW + '\nPasswords do not match';
     }
 
-    if (message !== "") {
-        alert(message);
-        return false;
+    if (this.messageUser) {
+      this.hiddenUN = false;
     }
-}
+    if (this.messagePassword) {
+      this.hiddenPW = false;
+    }
+
+    if (this.messageConfirmedPW) {
+      this.hiddenCPW = false;
+    }
+  }
 }
