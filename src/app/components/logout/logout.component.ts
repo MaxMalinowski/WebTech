e6b1a386b2f580d1fab155273060f23b9dccff5b
Max Malinowski <max.malinowski@outlook.de>
2021-11-24 07:19:28 +0100

    unset local storage items on logout

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-logout',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class LogoutComponent implements OnInit {
 
-  constructor() {}
+  constructor( private backendService: BackendService,) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.backendService.unsetUser()
+  }
 }
