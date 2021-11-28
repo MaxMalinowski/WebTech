3b731cdc17b2f76df1334aeae21463d27036c101
Max Malinowski <max.malinowski@outlook.de>
2021-11-28 12:36:19 +0100

    finished chat implementation

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,8 +1,11 @@
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
-import { Component, OnInit } from '@angular/core';
-import { AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Message } from 'src/app/models/Message';
+import { Profile } from 'src/app/models/Profile';
+import { User } from 'src/app/models/User';
 import { BackendService } from 'src/app/services/backend.service';
 import { ContextService } from 'src/app/services/context.service';
+import { IntervalService } from 'src/app/services/interval.service';
 
 @Component({
   selector: 'app-chat',
@@ -10,127 +13,86 @@ import { ContextService } from 'src/app/services/context.service';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit, AfterViewChecked {
-  dotContainer: HTMLElement | undefined;
-
-  // DIV für Nachrichten (s. Template) als Kind-Element für Aufrufe (s. scrollToBottom()) nutzen
+  public chatUsername: string;
+  public showLoadingIndicator: boolean = false
+  public allMessages: Message[] = []
+  public newMessage: string = '';
+  public showSingleLined: boolean = true;
   @ViewChild('messagesDiv') private myScrollContainer: ElementRef;
 
-  // Access chat username from context
-  public chatUsername: string = this.context.currentChatUsername;
-  public messageToSend: string = '';
-
-  messagesDisplayed: number = 0;
-  messagesOnServer: Array<Message> = [];
-
   public constructor(
+    private router: Router,
     private backendService: BackendService,
-    private context: ContextService
+    private context: ContextService,
+    private interval: IntervalService
   ) {
+    
     this.myScrollContainer = new ElementRef(null);
+    if (this.context.currentChatUsername != '') {
+      localStorage.setItem("chatUsername", this.context.currentChatUsername)
+    }
+    this.chatUsername = (localStorage.getItem('chatUsername') || '').toString()
+  }
+
+  public ngOnInit(): void {
+    this.getAllMessages();
+    this.getUserProfile();
   }
 
   public ngAfterViewChecked() {
     this.scrollToBottom();
   }
 
-  /**
-   * Setzt in der Nachrichtenliste die Scrollposition ("scrollTop") auf die DIV-Größe ("scrollHeight"). Dies bewirkt ein
-   * Scrollen ans Ende.
-   */
+  public ngOnDestroy(): void {
+    this.interval.clearIntervals()
+    localStorage.removeItem('chatUsername')
+}
+
+  private getAllMessages(): void {
+    const _this = this
+    this.interval.setInterval('getMessages', () => {
+      _this.backendService.listMessages(_this.chatUsername)
+      .then((messages: Message[]) => {
+        this.allMessages = []
+        messages.forEach((msg) => {
+          this.allMessages.push(new Message(msg.msg, msg.from, new Date((msg.time as any))))
+        })
+        this.showLoadingIndicator = false;
+        this.scrollToBottom();
+      })
+    })
+  }
+
+  private getUserProfile(): void {
+    this.backendService.loadUser(this.context.loggedInUsername)
+    .then((user: any) => {
+      let userProfile = user as Profile
+      if (userProfile.layout === 'double') {
+        this.showSingleLined = false;
+      } else {
+        this.showSingleLined = true;
+      }
+    })
+  }
+
   private scrollToBottom(): void {
     try {
-      this.myScrollContainer.nativeElement.scrollTop =
-        this.myScrollContainer.nativeElement.scrollHeight;
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
     } catch (err) {}
   }
 
-  public ngOnInit(): void {
-    this.scrollToBottom();
-  }
-
-  /**
-   * Update every second to check if new messages available
-   * --> Function not called explicitly, but continously every 1 second
-   */
-  /*  window.setInterval(function () {
-         this.recieveMessages();
-         if (this.messagesOnServer.length > this.messagesDisplayed) {
-             this.appendMessages(this.messagesOnServer.slice(this.messagesDisplayed));
-         }
-     }, 1000);*/
-
-  /**
-   * Send new message to the server
-   * --> Function used in HTML files
-   */
   public sendMessage(): void {
-    this.backendService.sendMessage(this.chatUsername, this.messageToSend);
-    this.insertLoadingIndicator();
+    this.backendService.sendMessage(this.chatUsername, this.newMessage);
+    this.newMessage = '';
+    this.showLoadingIndicator = true;
     this.scrollToBottom();
   }
 
-  public async showMessages(): Promise<void> {
-    var fieldsetElement = document.getElementById('chat');
-    var messages = await this.backendService.listMessages(this.chatUsername);
-    for (const msg of messages) {
-      const newElement = this.buildMessage(msg);
-      fieldsetElement?.appendChild(newElement);
-    }
-
-    this.removeLoadingIndicator();
-    this.scrollToBottom();
-  }
-
-  /**
-   * Build a param-element representing a message in the chat
-   */
-  public buildMessage(msg: Message): HTMLTableElement {
-    let newElement = document.createElement('table');
-    let newMsgName = document.createElement('td');
-    let newMsgText = document.createElement('td');
-    let newMsgDate = document.createElement('td');
-
-    newElement.classList.add('chat-message');
-    newMsgName.classList.add('chat-message-name');
-    newMsgText.classList.add('chat-message-text');
-    newMsgDate.classList.add('chat-message-date');
-
-    newMsgName.innerText = msg.from + ":";
-    newMsgText.innerText = msg.msg;
-    newMsgDate.innerText = new Date(msg.time).toLocaleString("en-DE");
-
-    newElement.appendChild(newMsgName);
-    newElement.appendChild(newMsgText);
-    newElement.appendChild(newMsgDate);
-
-    return newElement;
-  }
-
-  /**
-   * Insert a loading indicator
-   */
-  public insertLoadingIndicator(): void {
-    var fieldsetElement = document.getElementById('chat');
-
-    let newDotContainer = document.createElement('div');
-    let newFlashingDots = document.createElement('div');
-
-    newDotContainer.id = 'dot-container';
-    newDotContainer.classList.add('dot-container');
-    newFlashingDots.classList.add('dot-flashing');
-
-    newDotContainer.appendChild(newFlashingDots);
-    fieldsetElement?.appendChild(newDotContainer);
-  }
-
-  /**
-   * Remove a loading indicator
-   */
-  public removeLoadingIndicator(): void {
-    console.log('removing dots ...');
-    var exists = this.dotContainer;
-    if (exists != undefined && this.dotContainer) {
-      this.dotContainer.remove();
+  public removeFriend(): void {
+    let remove = confirm(`Do you really want to remove ${this.chatUsername} as friend?`)
+    if (remove) {
+      this.backendService.removeFriend(this.chatUsername)
+      this.router.navigate(['/friends'])
     }
   }
 }
