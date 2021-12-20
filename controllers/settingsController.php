c0e4cdb6590de98a21610e8d2f594c5e25574685
fbrueckl <fabian_brueckl@web.de>
2021-12-20 17:30:43 +0100

    finished settings.php and settingsController.php

--- controllers/settingsController.php
+++ controllers/settingsController.php
@@ -1,8 +1,44 @@
 <?php
 require("../utils/global.php");
 
+ini_set("display_errors", 1);
+
 if (!isset($_SESSION["user"])) {
     header("Location: login.php");
+}
+
+function loadUserSettings() {
+    global $service;
+    $user = $service->loadUser($_SESSION["user"]);
+
+    return $user;
+}
+
+function saveUserSettings() {
+    if (isset($_POST["firstName"])) {
+        global $service;
+        $user = $service->loadUser($_SESSION["user"]);
+
+        $username = $_SESSION["user"];
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $coffeeOrTea = $_POST["coffeeOrTea"];
+        $description = $_POST["description"];
+        $layout = $_POST["layout"];
+
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setCoffeeOrTea($coffeeOrTea);
+        $user->setDescription($description);
+        $user->setLayout($layout);
+
+        $result = $service->saveUser($user);
+        if ($result) {
+            header("Location: settings.php");
+        } else {
+            http_response_code(403);
+        }
+    }
 }
 
 ?>
--- pages/settings.php
+++ pages/settings.php
@@ -9,40 +9,48 @@
   </head>
 
   <body>
+    <?php $user = loadUserSettings() ?>
+    <?php saveUserSettings() ?>
     <h1>Profile Settings</h1>
 
     <!--textfields for name/surname and the dropdown menu for beverage selection-->
-    <form action="./settings.html" method="post">
+    <form action="" method="post">
       <fieldset>
         <legend>Base Data</legend>
 
         <div class="form-row">
           <div class="form-col-left">
-            <label for="first_name">First Name</label>
+            <label for="firstName">First Name</label>
           </div>
           <div class="form-col-right">
-            <input type="text" id="first_name" name="first_name" placeholder="Your name"/>
+            <input type="text" id="firstName" name="firstName" value="<?=$user->getFirstName()?>"  />
           </div>
         </div>
 
         <div class="form-row">
           <div class="form-col-left">
-            <label for="last_name">Last Name</label>
+            <label for="lastName">Last Name</label>
           </div>
           <div class="form-col-right">
-            <input type="text" id="last_name" name="last_name" placeholder="Your surname"/>
+            <input type="text" id="lastName" name="lastName" value="<?=$user->getLastName()?>"/>
           </div>
         </div>
 
         <div class="form-row">
           <div class="form-col-left">
-            <label for="coffee_or_tea">Coffee or Tea?</label>
+            <label for="coffeeOrTea">Coffee or Tea?</label>
           </div>
           <div class="form-col-right">
-            <select name="coffee_or_tea" id="coffee_or_tea">
-              <option value="neither_nor">Neither nor</option>
-              <option value="coffee">Coffee</option>
-              <option value="tea">Tea</option>
+            <select name="coffeeOrTea" id="coffeeOrTea">
+              <option value="neither_nor" <?php if($user->getCoffeeOrTea() == "neither_nor" ){?> selected <?php } ?>>
+                Neither nor
+              </option>
+              <option value="coffee" <?php if($user->getCoffeeOrTea() == "coffee" ){?> selected <?php } ?>>
+                Coffee
+              </option>
+              <option value="tea" <?php if($user->getCoffeeOrTea() == "tea" ){?> selected <?php } ?>>
+                Tea
+              </option>
             </select>
           </div>
         </div>
@@ -51,26 +59,28 @@
       <!--textfield for user comment-->
       <fieldset>
         <legend>Tell Something About You</legend>
-        <textarea name="about_you" placeholder="Leave a comment here"></textarea>
+        <textarea name="description" placeholder="Something about you..."><?=$user->getDescription()?></textarea>
       </fieldset>
 
       <!--radiobuttons to let the user choose their line settings-->
       <fieldset>
         <legend>Prefered Chat Layout</legend>
         <div class="radio-buttons">
-          <input type="radio" id="one_line" name="one_line" />
-          <label for="one_line">Username and message in one line</label>
+          <input type="radio" id="oneLine" name="layout" value="oneLine" 
+            <?php if($user->getLayout() == "oneLine" ){?> checked <?php } ?>/>
+          <label for="oneLine">Username and message in one line</label>
         </div>
         <div class="radio-buttons">
-          <input type="radio" id="seperated_lines" name="seperated_lines" />
-          <label for="seperated_lines">Username and message in separated lines</label>
+          <input type="radio" id="seperatedLines" name="layout" value="seperatedLines"
+            <?php if($user->getLayout() == "seperatedLines" ){?> checked <?php } ?> />
+          <label for="seperatedLines">Username and message in separated lines</label>
         </div>
       </fieldset>
 
       <!--buttons to save/cancel-->
       <div class="button-div">
-        <button type="submit" formaction="./friends.html">Cancel</button>
-        <button class="blue-button" type="submit">Save</button>
+        <button type="submit" formaction="./friends.php">Cancel</button>
+        <button class="blue-button" type="submit" >Save</button>
       </div>
     </form>
   </body>
