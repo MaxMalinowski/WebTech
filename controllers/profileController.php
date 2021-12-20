27dec002e231531b062cb9319b52b9cd1c35bd44
fbrueckl <fabian_brueckl@web.de>
2021-12-20 20:46:40 +0100

    started implementing profile.php

--- controllers/profileController.php
+++ controllers/profileController.php
@@ -5,4 +5,14 @@ if (!isset($_SESSION["user"])) {
     header("Location: login.php");
 }
 
+if (!isset($_GET["friend"])) {
+    header("Location: chat.php");
+}
+
+function loadUserInfo() {
+    global $service;
+    $friend = $service->loadUser($_GET["friend"]);
+    return $friend;
+}
+
 ?>
