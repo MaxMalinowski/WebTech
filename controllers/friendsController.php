77da8dddccfb80ceaabb7370ffaa310e0f72e950
lina0987 <li.st99@web.de>
2021-12-22 12:04:29 +0100

    added counter fror unread

--- controllers/friendsController.php
+++ controllers/friendsController.php
@@ -1,65 +1,78 @@
 <?php
+
+use model\Friend;
+
 require("../utils/global.php");
 
-function checkForPresentUser() {
+function checkForPresentUser()
+{
     if (!isset($_SESSION["user"])) {
-        header("Location: login.php"); 
+        header("Location: login.php");
     }
 }
 
-function processFriendRequests() {
+function processFriendRequests()
+{
     if ((isset($_POST['action']) && isset($_POST['username'])) || (isset($_GET['action']) && isset($_GET['username']))) {
         global $service;
         $action = isset($_POST['action']) ? $_POST['action'] : $_GET['action'];
         $username = isset($_POST['username']) ? $_POST['username'] : $_GET['username'];
-            
+
         switch ($action) {
             case 'add-friend':
                 $service->userExists($username)
-                ? ($service->friendRequest($username)
-                    ? header("Location: friends.php")
-                    : http_response_code(500))
-                :http_response_code(404);
+                    ? ($service->friendRequest($username)
+                        ? header("Location: friends.php")
+                        : http_response_code(500))
+                    : http_response_code(404);
                 break;
 
             case 'accept-friend':
-                $service->friendAccept($username) 
-                ? header("Location: friends.php")
-                : http_response_code(500); 
+                $service->friendAccept($username)
+                    ? header("Location: friends.php")
+                    : http_response_code(500);
                 break;
-            
+
             case 'dismiss-friend':
                 $service->friendDismiss($username)
-                ? header("Location: friends.php")
-                : http_response_code(500); 
+                    ? header("Location: friends.php")
+                    : http_response_code(500);
                 break;
-            
+
             case 'remove-friend':
                 $service->friendRemove($username)
-                ? header("Location: friends.php")
-                : http_response_code(500); 
+                    ? header("Location: friends.php")
+                    : http_response_code(500);
                 break;
-            
+
             default:
                 break;
-        }  
+        }
     }
 }
 
-function getFriendsInformation() {
+function getFriendsInformation()
+{
     global $service;
     $friendsList = [];
     $requestsList = [];
 
     $allFriends = $service->listFriends();
-    foreach($allFriends as $friend) {
+    foreach ($allFriends as $friend) {
         if ($friend->getStatus() == 'accepted') {
             array_push($friendsList, $friend);
         } elseif ($friend->getStatus() == 'requested') {
             array_push($requestsList, $friend);
         }
     }
+    $allUnread = $service->getUnread();
+    foreach ($allUnread as $unreadfriend => $unreadCount) {
+        foreach ($friendsList as $friend) {
+            if ($unreadfriend === $friend->getUsername()) {
+                $friendsList[$friend->getUsername()]  = $unreadCount;
+            }
+        }
+    }
 
     return array($friendsList, $requestsList);
 }
-?>
